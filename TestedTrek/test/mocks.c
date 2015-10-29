87162d99595d97f621dc5b21121b79504517e634
Rob Myers <Rob.Myers@agileInstitute.com>
2015-10-29 07:25:32 -0700

    found the problem, but haven't fixed it
    somehow on Ubuntu printf is becoming puts.

--- TestedTrek/test/mocks.c
+++ TestedTrek/test/mocks.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
+/*
+ifdef Ubuntu for puts
+why is printf becoming puts? Goog!
+*/
+
 char mockOutput[256];
 
+extern int puts(const char *myString) {
+	fprintf(stdout, "MOCKED puts!");
+	fprintf(stderr, "MOCKED puts!");
+}
 
 extern int printf (__const char *__restrict __format, ...)
 {
-	fprintf(stdout, "MOCKED!");
-	fprintf(stderr, "MOCKED!");
+	fprintf(stdout, "MOCKED printf!");
+	fprintf(stderr, "MOCKED printf!");
 	return 0;
 }
