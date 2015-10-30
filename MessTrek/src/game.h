5c7d89da679790b365c4cdd1fb26c1edfc5a581d
Rob Myers <Rob.Myers@agileInstitute.com>
2015-10-29 18:54:00 -0700

    synched up MessTrek legacy code with TestedTrek code
    MessTrek and TestedTrek src code now looks identical. This is prior to
    making changes to TestedTrek src for my addition of characterization
    tests, for the refactoring lab.

--- MessTrek/src/game.h
+++ MessTrek/src/game.h
@@ -1,7 +1,11 @@
 #ifndef GAME_HEADER 
 #define GAME_HEADER 
 
+#include "klingon.h"
+
 #define RND(maximum) (rand() % maximum)
 #define streq(a, b) (strcmp(a, b) == 0)
+
+void fireWeapon(char *command, char *parameter, struct Klingon *enemy);
 
 #endif
--- MessTrek/src/klingon.h
+++ MessTrek/src/klingon.h
@@ -1,10 +1,15 @@
 #ifndef KLINGON_HEADER
 #define KLINGON_HEADER
 
+#include <stdlib.h>
+
 typedef struct Klingon {
 	int distance;
 	int energy;
 	int destroyed;
 } Klingon;
+
+
+void initKlingon(Klingon *k);
 
 #endif
